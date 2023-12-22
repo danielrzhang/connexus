70ba5d5ddeba3f78a6d81f94beb13c609b852a82
Daniel Zhang <danielrzhang@gmail.com>
2023-12-22 17:45:16 -0500

    Updated formatting

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,27 +1,29 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+      default: "https://icons8.com/icon/teAmm8wzAnK7/test-account",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  picture: {
-    type: String,
-    required: true,
-    default: "https://icons8.com/icon/teAmm8wzAnK7/test-account"
-  },
-},
-{
-  timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
