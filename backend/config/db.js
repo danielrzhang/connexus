f0fddadc21eb271806bfa2d0df583c9484473f15
Daniel Zhang <danielrzhang@gmail.com>
2023-12-22 23:06:42 -0500

    Connected MongoDB to backend

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red.bold);
+    process.exit();
+  }
+};
+
+module.exports = connectDB;
