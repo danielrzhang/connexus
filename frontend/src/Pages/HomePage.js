1a0f60dfd263e7829a06e8479dd1f4cad5401c56
Daniel Zhang <danielrzhang@gmail.com>
2023-12-22 22:30:58 -0500

    Added login and signup windows

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,58 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        display="flex"
+        justifyContent="center"
+        padding={3}
+        backgroundColor={"white"}
+        width="100%"
+        margin="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work Sans" color="black">
+          Connexus
+        </Text>
+      </Box>
+      <Box
+        backgroundColor={"white"}
+        width="100%"
+        padding={4}
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList marginBottom="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
